f2a36e81d5a08ece630347e713a7a9b19ee2ca80
Monk <monkchen@apjcorp.com>
2019-01-08 13:42:07 +0800

    Xamarin.Android.QMUI

---
+++ Xamarin.Android.QMUI/src/Xamarin.Android.QMUI/Xamarin.Android.QMUI/Additions/QMUITouchableSpan.cs
@@ -0,0 +1,13 @@
+﻿using Com.Qmuiteam.Qmui.Link;
+using System;
+
+namespace Com.Qmuiteam.Qmui.Span
+{
+    public abstract partial class QMUITouchableSpan
+    {
+        void ITouchableSpan.SetPressed(bool p0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
